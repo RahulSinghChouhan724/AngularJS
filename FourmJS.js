2b82282e3f035e82669e38aa6c71eb11d299eb3e
unknown <mailtorahul724@gmail.copm>
2019-02-19 12:57:29 +0530

    data fetching Working

--- FourmJS.js
+++ FourmJS.js
@@ -3,42 +3,39 @@
 var FourmApp= angular.module('MyApp',[]);
  //Upload File Directive
 
-
-FourmApp.controller('FourmController',function($scope,$http){
-
+FourmApp.controller('FourmController',function($scope){
+//    variable
    $scope.City=[
         {name:"Bangalore"},
         {name:"Mumbai"},
         {name:"delhi"}
     ];
     
-    // Fetcher
-    $scope.update=function(Room,){
+    // Image Fetcher
+    $scope.updateImage=function(Room){
         $scope.master =angular.copy(Room);
-        // $scope.temp.push($scope.master);
+        return  $scope.master;
+    }
+    //feild Fetcher
+    $scope.submite= function(Room){
         var item = {};
+        $scope.Data = [];
+
+
         item.name = $scope.Room.name;
         item.cmnt =$scope.Room.Comment;
         item.City =$scope.Room.City;
         item.HT = $scope.Room.radio;
+        item.RI=$scope.master;
+        
+        this.Data.push(item);
 
-        $scope.onFileSelect = function($files) {
-            Upload.upload({
-              url: 'my/upload/url',
-              file: $files,            
-            }).progress(function(e) {
-            }).then(function(data, status, headers, config) {
-              // file is uploaded successfully
-              console.log(data);
-            }); 
-        item.RI= $scope.data;
-        $scope.temp.push(item);
+        
         if($scope.form.$valid){
             alert('Data Succesfully Submit!!')
         }
+        return item;
     }
-
-  
 });
 
 
