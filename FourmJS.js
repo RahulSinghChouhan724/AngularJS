f84e37433e1508e092bc7a54c7e4282e70199064
unknown <mailtorahul724@gmail.copm>
2019-02-19 19:32:35 +0530

    Fetcher And loader form with modal

--- FourmJS.js
+++ FourmJS.js
@@ -2,46 +2,50 @@
 
 var FourmApp= angular.module('MyApp',[]);
  //Upload File Directive
-
-FourmApp.controller('FourmController',function($scope){
-//    variable
-   $scope.City=[
-        {name:"Bangalore"},
-        {name:"Mumbai"},
-        {name:"delhi"}
-    ];
-    
-    // Image Fetcher
-    $scope.updateImage=function(Room){
-        $scope.master =angular.copy(Room);
-        return  $scope.master;
-    }
-    //feild Fetcher
-    $scope.submite= function(Room){
-        var item = {};
-        $scope.Data = [];
-
-
-        item.name = $scope.Room.name;
-        item.cmnt =$scope.Room.Comment;
-        item.City =$scope.Room.City;
-        item.HT = $scope.Room.radio;
-        item.RI=$scope.master;
-        
-        this.Data.push(item);
-
-        
-        if($scope.form.$valid){
-            alert('Data Succesfully Submit!!')
+ FourmApp.directive('fileRead', [ function() {
+    return {
+        restrict : 'A',
+        scope : {
+            fileModel : '='
+        },
+        link : function(scope, elem, attr) {
+            elem.bind('change', function(changeEvent) {
+                var reader = new FileReader();
+                reader.onload = function(loadEvent) {
+                    scope.$apply(function() {
+                        scope.fileModel = loadEvent.target.result;
+                    });
+                };
+                reader.readAsDataURL(changeEvent.target.files[0]);
+            });
         }
-        return item;
     }
-});
+} ])
 
-
-    
+FourmApp.controller('FourmController',['$scope',
+ function($scope,$user){
+            
+            $scope.City=[
+                {key:"",value:"select city"},
+                {key:"Bangalore",value:"Bangalore"},
+                {key:"Mumbai",value:"Mumbai"},
+                {key:"delhi",value:"delhi"}
+            ];
+            $scope.Data = [];
+            //feild Fetcher
+            $scope.submite= function(Room){
+                var item = {};
+                item.name = $scope.Room.name;
+                item.cmnt =$scope.Room.Comment;
+                item.City =$scope.Room.City;
+                item.HT = $scope.Room.radio;
+                item.RI = $scope.Room.inputFile;
+                // item.RI=$scope.fd;
+                $scope.Data.push(item); 
+                // var my =JSON.stringify(item);
+                
+               $scope.Room.name="";
+            }
+        }     
  
-
-
-
-
+]);
